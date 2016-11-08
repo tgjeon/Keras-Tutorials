d6c7b30070ca7d7cfa8858979903fcb4473a81cb
Taegyun Jeon <tgjeon@satreci.com>
2016-11-08 16:29:22 +0900

    Add deep MLP

---
+++ 04_modern_net.py
@@ -0,0 +1,39 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.optimizers import RMSprop
+from keras.datasets import mnist
+from keras.utils import np_utils
+
+batch_size = 128
+nb_classes = 10
+nb_epoch = 100
+
+# Load MNIST dataset
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+# Logistic regression model
+model = Sequential()
+model.add(Dense(output_dim=625, input_dim=784, init='normal'))
+model.add(Activation('relu'))
+model.add(Dense(output_dim=625, input_dim=625, init='normal'))
+model.add(Activation('relu'))
+model.add(Dense(output_dim=10, input_dim=625, init='normal'))
+model.add(Activation('softmax'))
+
+model.compile(optimizer=RMSprop(lr=0.001, rho=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+# Train
+history = model.fit(X_train, Y_train, nb_epoch=nb_epoch, batch_size=batch_size, verbose=1)
+
+# Evaluate
+evaluation = model.evaluate(X_test, Y_test, verbose=1)
+print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (evaluation[0], evaluation[1]))
