ae32cc2066e07bb9ab6fee788f78a6d454905c5e
Taegyun Jeon <tgjeon@satreci.com>
2016-11-10 14:16:26 +0900

    CNN with 5 layers (3 Conv and  2 Fc)

---
+++ 05_convolutional_net.py
@@ -0,0 +1,84 @@
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Convolution2D, MaxPooling2D, Dropout
+from keras.optimizers import RMSprop
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras import initializations
+from keras import backend as K
+
+batch_size = 128
+nb_classes = 10
+nb_epoch = 100
+
+img_rows, img_cols = 28, 28         # input image dimensions
+pool_size = (2, 2)                  # size of pooling area for max pooling
+prob_drop_conv = 0.2                # drop probability for dropout @ conv layer
+prob_drop_hidden = 0.5              # drop probability for dropout @ fc layer
+
+def init_weights(shape, name=None):
+    return initializations.normal(shape, scale=0.01, name=name)
+
+# Load MNIST dataset
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+print('X_train original shape:', X_train.shape)
+
+if K.image_dim_ordering() == 'th':
+    # For Theano backend
+    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    # For TensorFlow backend
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+
+# Convolutional model
+model = Sequential()
+
+# conv1 layer
+model.add(Convolution2D(32, 3, 3, border_mode='same', activation='relu', input_shape=input_shape, init=init_weights))
+model.add(MaxPooling2D(pool_size=pool_size, strides=(2,2), border_mode='same'))
+model.add(Dropout(prob_drop_conv))
+
+# conv2 layer
+model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu', init=init_weights))
+model.add(MaxPooling2D(pool_size=pool_size, strides=(2,2), border_mode='same'))
+model.add(Dropout(prob_drop_conv))
+
+# conv3 layer
+model.add(Convolution2D(128, 3, 3, border_mode='same', activation='relu', init=init_weights))
+model.add(MaxPooling2D(pool_size=pool_size, strides=(2,2), border_mode='same'))
+model.add(Flatten())
+model.add(Dropout(prob_drop_conv))
+
+# fc1 layer
+model.add(Dense(625, activation='relu', init=init_weights))
+model.add(Dropout(prob_drop_hidden))
+
+# fc2 layer
+model.add(Dense(10, activation='softmax', init=init_weights))
+
+opt = RMSprop(lr=0.001, rho=0.9)
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+# Train
+history = model.fit(X_train, Y_train, nb_epoch=nb_epoch, batch_size=batch_size, shuffle=True, verbose=1)
+
+# Evaluate
+evaluation = model.evaluate(X_test, Y_test, batch_size=256, verbose=1)
+print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (evaluation[0], evaluation[1]))
