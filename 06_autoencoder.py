fd2db67a286dbe34a12d6d7dad8aa9dc8900d7a2
Taegyun Jeon <tgjeon@satreci.com>
2016-11-15 16:27:29 +0900

    Add autoencoder

---
+++ 06_autoencoder.py
@@ -0,0 +1,50 @@
+from keras.models import Model
+from keras.layers import Dense, Input
+from keras.datasets import mnist
+import numpy as np
+
+# Hyper parameters
+batch_size = 128
+nb_epoch = 100
+
+# Parameters for MNIST dataset
+img_rows, img_cols = 28, 28
+
+# Parameters for denoising autoencoder
+nb_visible = img_rows * img_cols
+nb_hidden = 500
+corruption_level = 0.3
+
+# Load MNIST Dataset
+(x_train, _), (x_test, _) = mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print(x_train.shape)
+print(x_test.shape)
+
+# Add random noise
+x_train_noisy = x_train + corruption_level * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
+x_test_noisy = x_test + corruption_level * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+print(x_train_noisy.shape)
+print(x_test_noisy.shape)
+
+# Build autoencoder model
+input_img = Input(shape=(nb_visible,))
+encoded = Dense(nb_hidden, activation='relu')(input_img)
+decoded = Dense(nb_visible, activation='sigmoid')(encoded)
+
+autoencoder = Model(input=input_img, output=decoded)
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+# Train
+autoencoder.fit(x_train_noisy, x_train,
+                nb_epoch=nb_epoch, batch_size=batch_size, shuffle=True, verbose=1,
+                validation_data=(x_test_noisy, x_test))
+
+# Evaluate
+evaluation = autoencoder.evaluate(x_test_noisy, x_test, batch_size=batch_size, verbose=1)
+print('\nSummary: Loss over the test dataset: %.2f' % evaluation)
